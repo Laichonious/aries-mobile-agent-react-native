722ee3b9313ca6e8c03927d25b5ce31df2a81771
Clément Humbert <clement.humbert@sicpa.com>
2021-12-01 10:54:03 +0100

    Add sovrin BuilderNet ledger configuration
    Signed-off-by: Clément Humbert <clement.humbert@sicpa.com>

--- configs/ledgers/indy/index.js
+++ configs/ledgers/indy/index.js
@@ -1,4 +1,5 @@
 import IndicioTestNet from './indicio-test-net/pool-config'
 import SovrinStagingNet from './sovrin-staging-net/pool-config'
+import SovrinBuilderNet from './sovrin-builder-net/pool-config'
 
-export default [IndicioTestNet, SovrinStagingNet]
+export default [IndicioTestNet, SovrinStagingNet, SovrinBuilderNet]
